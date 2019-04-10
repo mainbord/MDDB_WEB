1b18a5175f065347f87faec77728bff1fda4a970
mainbord <n56016gb@gmail.com>
2019-04-11 01:19:17 +0300

    logger fix

--- src/main/java/com/mddb/controller/WebController.java
+++ src/main/java/com/mddb/controller/WebController.java
@@ -5,7 +5,6 @@ import com.mddb.service.DbService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -15,7 +14,7 @@ import java.util.Map;
 
 @Controller
 @RequiredArgsConstructor
-@Log4j2
+@Log4j2(topic = "app")
 public class WebController {
 
     private final DbService service;
