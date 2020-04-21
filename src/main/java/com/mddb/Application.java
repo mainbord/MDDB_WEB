0fc9649fcbfae92c43ed68330bf48dceec5d531d
Maxim Lebedev <n56016gb@gmail.com>
2020-04-22 00:51:12 +0300

    spring boot update, loader refactoring

--- src/main/java/com/mddb/Application.java
+++ src/main/java/com/mddb/Application.java
@@ -2,13 +2,13 @@ package com.mddb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableAsync
 public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
-//        ConfigurableApplicationContext context = SpringApplication.run(Application.class, args);
-//        context.getBean(DbServicesImpl.class).saveDevices();
     }
 }
--- src/main/java/com/mddb/usecase/impl/LoaderServiceImpl.java
+++ src/main/java/com/mddb/usecase/impl/LoaderServiceImpl.java
@@ -5,6 +5,7 @@ import com.mddb.dao.DeviceRepository;
 import com.mddb.usecase.DeviceLoader;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -16,7 +17,9 @@ public class LoaderServiceImpl implements DeviceLoader {
     private final DeviceClient deviceClient;
 
     @Override
+    @Async
     public void loadDevices() {
+        log.info("loadDevices started");
         repository.saveAll(deviceClient.getDevices());
         log.info("ALL devices saved");
     }
